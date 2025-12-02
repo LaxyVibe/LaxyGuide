a0bcdb4984fffd00003d07b29bc3bfc728251f15
Sun <sun@laxy.travel>
2025-12-02 18:02:24 +0800

    netlify login

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import { Buffer } from 'buffer';
+// Polyfill Buffer for gray-matter
+// @ts-ignore
+globalThis.Buffer = Buffer;
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
