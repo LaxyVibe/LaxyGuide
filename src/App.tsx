a5980d14527509f0cad3abe9dd7a3db3a487b3c1
Sun <sun@laxy.travel>
2025-12-19 15:29:03 +0800

    staging

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
       <AnalyticsTracker />
       <Routes>
         {/* Handle /admin route explicitly to bypass SPA routing and load static admin page */}
-        <Route path="/laxy-admin" element={<AdminRedirect />} />
+        {/* <Route path="/" element={<AdminRedirect />} /> */}
         <Route path="/" element={<GuideListing />} />
         <Route path="/:guideId" element={<Landing />} />
         <Route path="/:guideId/list" element={<POIListing />} />
